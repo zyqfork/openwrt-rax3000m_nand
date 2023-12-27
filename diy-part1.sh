56af5591230538632ac2a36cbaaa965a0e0b0d11
zouyq <zyqcn@live.com>
2023-12-27 20:19:10 +0800

    update 237 .config

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,12 @@
 #echo 'src-git oaf https://github.com/destan19/OpenAppFilter.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+
+rm -rf feeds/packages/net/v2ray-geodata
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
