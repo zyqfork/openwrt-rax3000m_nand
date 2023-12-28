ff6e5925ed348fba77ed598c5ff70f947a5ceac7
zouyq <zyqcn@live.com>
2023-12-28 09:03:45 +0800

    update diy

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,13 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
+# add mosdns
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+
+rm -rf feeds/packages/net/v2ray-geodata
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
